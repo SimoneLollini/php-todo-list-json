62233d4449eb3b8401044a67f3781d1bc5807b50
SimoneLollini <lollinisimone@gmail.com>
2022-11-30 17:41:58 +0100

    Add Tasks from tasks.json

--- server.php
+++ server.php
@@ -1,18 +1,14 @@
 <?php
-$tasks_string = [
-    "Learn PHP",
-    "Learn Mysql",
-    "Learn Laravel",
-    "Read a book",
-    "read kubernetes book",
-    "read kubernetes book",
-    "new task"
-];
-$tasks = json_encode($tasks_string, true);
+// $tasks_string = [
+//     "Learn PHP",
+//     "Learn Mysql",
+//     "Learn Laravel",
+//     "Read a book",
+//     "read kubernetes book",
+//     "read kubernetes book",
+//     "new task"
+// ];
+
+$tasks = file_get_contents('tasks.json');
 
 echo $tasks;
-
-// $tasks_string = file_get_contents('tasks.json');
-// var_dump($tasks_string);
-// $tasks = json_decode($tasks_string);
-// var_dump($tasks);
